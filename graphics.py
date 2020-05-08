f4d5159a1cd65965b138ccbc78a1c1cb7a5663df
snytav <snytav@gmail.com>
2020-05-09 00:34:22 +0700

    phase planes drawn up to 15

--- graphics.py
+++ graphics.py
@@ -3,6 +3,7 @@ import math
 
 def phase_space(t,dt,r,v):
     if math.floor(t / dt) % math.floor(5 / dt) == 0:
+       plt.figure()
        plt.scatter(r,v,s=1)
        plt.xlabel('X')
        plt.ylabel('Y')
