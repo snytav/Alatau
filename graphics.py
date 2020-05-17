1ffe88ab192efe27bec3480e19856f08d3097de0
snytav <snytav@gmail.com>
2020-05-17 14:06:34 +0700

    phase planes for verification

--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 def phase_space(t,dt,r,v):
     if math.floor(t / dt) % math.floor(5 / dt) == 0:
@@ -8,5 +9,7 @@ def phase_space(t,dt,r,v):
        plt.xlabel('X')
        plt.ylabel('Y')
        tit = 'Electron Phase-space distribution for t = ' + str(t)
+       fname = 'phase-space_'+str(math.floor(t / dt))
+       np.savetxt(fname, np.concatenate((r,v)) , fmt='%15.5e')
        plt.title(tit)
        plt.show()
