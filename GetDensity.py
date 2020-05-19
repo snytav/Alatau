862266a51cadec53634cd5b8ee74f5b9d23be3e4
snytav <snytav@gmail.com>
2020-05-19 18:04:00 +0700

    GetDensity with NumPy operations

--- GetDensity.py
+++ GetDensity.py
@@ -3,12 +3,12 @@ import numpy as np
 def GetDensity( r, L, J ):
 # Evaluate number density n in grid of J cells, length   L, from the electron positions r
 
-    dx = L/J
+    dx = np.divide(L,J)
     #return dx
 
-    js = np.floor(r/dx)
+    js = np.floor(np.divide(r,dx))
     #return js
-    ys = r/dx - (js)
+    ys = np.divide(r,dx) - (js)
     #js -= 1
     #return ys
     n1 =  ys
@@ -19,12 +19,12 @@ def GetDensity( r, L, J ):
 
 
     #return js_plus_1
-    n1 = accum(js.astype(int),(1-ys)/dx)
+    n1 = accum(js.astype(int),np.divide((1-ys),dx))
     np.savetxt('n1.txt', n1, delimiter='\n',fmt='%15.5e')
     np.savetxt('js.txt', js+1,delimiter='\n',fmt='%15d')
     np.savetxt('js_plus_1.txt', js_plus_1+1, delimiter='\n',fmt='%15d')
     np.savetxt('ys_dx.txt', ys/dx, delimiter='\n',fmt='%15.5e')
-    n2 = accum(js_plus_1.astype(int),ys/dx)
+    n2 = accum(js_plus_1.astype(int),np.divide(ys,dx))
     np.savetxt('n2.txt', n2, delimiter='\n',fmt='%15.5e')
     n = n1 + n2
 
