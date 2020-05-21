6b51470977748830e8d75b79d1c7b720062033f7
snytav <snytav@gmail.com>
2020-05-21 21:08:50 +0700

    uniform particle distribution

--- driverPIC.py
+++ driverPIC.py
@@ -30,7 +30,7 @@ from GetDensity import GetDensity
 
 from test_density import test_density
 
-#test_density()
+test_density()
 
 L = 100      # domain of solution 0 <= x <= L
 N = 20000    # number of electrons
